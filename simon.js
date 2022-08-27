8919b98778ccbf8a371ebbfd57aa52010553f7f0
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-08-27 16:16:16 +0530

    Game complete Commit

--- simon.js
+++ simon.js
@@ -4,14 +4,68 @@ var buttonColours=["red","blue","green","yellow"];
 var gamepattern=[];
 var userClickedPattern=[];
 
+var started=false;
+var level=0;
+$(Document).keypress(function(){
+    if(!started){
+        $('#level-title').text(`Level ${level}`);
+        nextSequence();
+        started=true;
+
+    }
+});
+
 $('.btn').click(function(){
     var userChosenColour=$(this).attr('id');
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);    
 });
 
+function checkAnswer(currentLevel) {
+
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamepattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamepattern.length){
+
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      playSound("wrong");
+
+      $("body").addClass("game-over")
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart")
+   
+   
+    startOver();
+    }
+
+}
+
 function nextSequence(){
+    userClickedPattern=[];
+    
+    level++;
+    $('#level-title').text(`Level ${level}`);
+
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColour=buttonColours[randomNumber];
     gamepattern.push(randomChosenColour);
@@ -36,3 +90,9 @@ function animatePress(currentColour){
         $("#"+currentColour).removeClass("pressed");
     },100);
 };
+
+function startOver(){
+    level=0;
+    gamepattern=[];
+    started=false;
+}
